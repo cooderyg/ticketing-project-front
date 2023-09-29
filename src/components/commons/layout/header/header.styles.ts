8d9f695941a7b26ae8b7bea9920bb9d4d439b088
cooderyg <ygk9604@naver.com>
2023-09-29 17:19:20 +0900

    feat: header logo header 로고 추가

--- src/components/commons/layout/header/header.styles.ts
+++ src/components/commons/layout/header/header.styles.ts
@@ -1,13 +1,21 @@
 import styled from "@emotion/styled";
+import { color } from "../../../../commons/styles/color.styles";
 
 export const Header = styled.header`
   border-bottom: 1px solid #ccc;
 `;
 
 export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   max-width: 1280px;
   margin: 0 auto;
-  padding: 4px 0;
+  padding: 8px 12px 8px 20px;
+`;
+
+export const LogoBox = styled.h1`
+  cursor: pointer;
 `;
 
 export const Nav = styled.nav`
@@ -15,5 +23,10 @@ export const Nav = styled.nav`
   gap: 4px;
   > a {
     padding: 12px 8px;
+    font-weight: bold;
+    transition: 0.2s;
+    :hover {
+      color: ${color.gray};
+    }
   }
 `;
--- src/components/commons/layout/header/index.tsx
+++ src/components/commons/layout/header/index.tsx
@@ -1,10 +1,23 @@
 import Link from "next/link";
 import * as S from "./header.styles";
+import Image from "next/image";
+import LogoImage from "../../../../../public/images/ticketing-logo.jpg";
 
 export default function LayoutHeader(): JSX.Element {
   return (
     <S.Header>
       <S.Wrapper>
+        <S.LogoBox>
+          <Link href="/">
+            <Image
+              src={LogoImage}
+              alt="logo"
+              width={120}
+              height={40}
+              unoptimized={true}
+            />
+          </Link>
+        </S.LogoBox>
         <S.Nav>
           <Link href="">
             <a>뮤지컬</a>
