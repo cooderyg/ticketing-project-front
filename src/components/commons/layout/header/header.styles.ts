64e8a66fc7aa39a530f3a8f7b98a023e9b0e2cb6
cooderyg <ygk9604@naver.com>
2023-10-17 22:29:06 +0900

    feat: user menu markup user menu 마크업 css 완료

--- src/components/commons/layout/header/header.styles.ts
+++ src/components/commons/layout/header/header.styles.ts
@@ -27,7 +27,9 @@ export const LogoBox = styled.h1`
 `;
 
 export const Nav = styled.nav`
+  position: relative;
   display: flex;
+  align-items: center;
   gap: 4px;
   > a {
     padding: 12px 8px;
@@ -39,10 +41,77 @@ export const Nav = styled.nav`
   }
 `;
 
+export const ProfileImageBox = styled.div`
+  width: 36px;
+  height: 36px;
+  border: 1px solid ${color.gray};
+  border-radius: 50px;
+  overflow: hidden;
+  cursor: pointer;
+`;
+
+export const UserMenuBox = styled.ul`
+  z-index: 999;
+  position: absolute;
+  top: 44px;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 12px;
+  width: 180px;
+  padding: 12px;
+  border: 1px solid ${color.gray};
+  border-radius: 4px;
+  background-color: ${color.white};
+  li,
+  button,
+  a,
+  span {
+    font-size: 12px;
+    font-weight: bold;
+    color: ${color.black};
+  }
+`;
+
+export const MyWrapper = styled.li`
+  display: flex;
+  width: 100%;
+`;
+
+export const Nickname = styled.span`
+  flex: 1;
+`;
+
+export const Mypage = styled.a`
+  display: inline-block;
+  transition: 0.2s;
+  cursor: pointer;
+  :hover {
+    color: ${color.primary};
+  }
+`;
+
+export const PointWrapper = styled.li`
+  display: flex;
+  width: 100%;
+`;
+
+export const NowPoint = styled.span`
+  flex: 1;
+`;
+
+export const ChargeButton = styled.button`
+  transition: 0.2s;
+  cursor: pointer;
+  :hover {
+    color: ${color.primary};
+  }
+`;
+
+export const LogoutButtonWrapper = styled.li``;
+
 export const LogoutButton = styled.button`
-  padding: 12px 8px;
-  font-weight: bold;
-  color: ${color.black};
   transition: 0.2s;
   cursor: pointer;
   :hover {
